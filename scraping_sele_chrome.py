9e4dca8b8fe65b9a8e3a81040f5bf7017d4739ff
gitgetgotguts <getgetgit@proton.me>
2024-08-23 23:38:45 +0100

    selenium manager integrated

--- scraping_sele_chrome.py
+++ scraping_sele_chrome.py
@@ -2,24 +2,26 @@
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-driver = uc.Chrome()
+driver = webdriver.Chrome()
 # Open a webpage
 driver.get("http://quotes.toscrape.com/")
 L=[]
 
-tquotes=[x.text for x  in driver.find_elements(By.CLASS_NAME,"quote")]
-L.append(tquotes)
+# tquotes=[x.text for x  in driver.find_elements(By.CLASS_NAME,"quote")]
+# L.append(tquotes)
 oc=0
 while 1:
     try:
-        btn=driver.find_element(By.CLASS_NAME,'next').find_element(By.TAG_NAME,'a')
-        btn.click()
         tquotes=[x.text for x  in driver.find_elements(By.CLASS_NAME,"quote")]
         L.append(tquotes)
+        btn=driver.find_element(By.CLASS_NAME,'next').find_element(By.TAG_NAME,'a')
+        btn.click()
+        
         oc+=2
     except:
         print(L)
         driver.close()
+        break
 
 # btn.click()
 # tmp=[x.text for x  in driver.find_elements(By.CLASS_NAME,"quote")]
--- scraping_sele_firefox.py
+++ scraping_sele_firefox.py
@@ -1,20 +1,15 @@
 #first attempt  clicking button then looping the elements
-import undetected_chromedriver as uc
-from selenium.webdriver.firefox.service import Service
+
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 L=[]
 
 
-# Specify the path to the geckodriver executable
-service = Service(executable_path='/bin/geckodriver')
-driver = webdriver.Firefox(service=service)
+
+driver = webdriver.Firefox()
 # Open a webpage
 driver.get("http://quotes.toscrape.com/")
-# tquotes=[x.text for x  in driver.find_elements(By.CLASS_NAME,"quote")]
-# L.append(tquotes)
-
 while 1:
     try:
         
@@ -26,5 +21,6 @@ while 1:
     except:
         print(L)
         driver.quit()
+        break
 
 
