5e4fd0306c0ae0165b55b02847e19c0b504f0202
gitgetgotguts <getgetgit@proton.me>
2024-08-23 17:54:10 +0100

    set up with firefox
    it s faster with firefox

--- finalfirefox.py
+++ finalfirefox.py
@@ -12,12 +12,18 @@ service = Service(executable_path='/bin/geckodriver')
 driver = webdriver.Firefox(service=service)
 # Open a webpage
 driver.get("http://quotes.toscrape.com/")
-tmpl=[x.text for x  in driver.find_elements(By.CLASS_NAME,"quote")]
-L.append(tmpl)
-btn=driver.find_element(By.CLASS_NAME,'next').find_element(By.TAG_NAME,'a')
-btn.click()
-tmp=[x.text for x  in driver.find_elements(By.CLASS_NAME,"quote")]
+tquotes=[x.text for x  in driver.find_elements(By.CLASS_NAME,"quote")]
+L.append(tquotes)
 
-print(tmp)
+while 1:
+    try:
+        btn=driver.find_element(By.CLASS_NAME,'next').find_element(By.TAG_NAME,'a')
+        btn.click()
+        tquotes=[x.text for x  in driver.find_elements(By.CLASS_NAME,"quote")]
+        L.append(tquotes)
+        
+    except:
+        print(L)
+        driver.close()
 
-driver.close()
+
